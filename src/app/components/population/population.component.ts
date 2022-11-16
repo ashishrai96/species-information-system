65a1de0c5afa3efe1a58984ade1f80fb7647c442
Ashish Rai <ashishrai96@yahoo.com>
2022-11-17 02:09:14 +0530

    population graphs added

--- src/app/components/population/population.component.ts
+++ src/app/components/population/population.component.ts
@@ -2,53 +2,93 @@ import { Component, OnInit } from '@angular/core';
 import { GlobalService } from 'src/app/service/global.service';
 
 @Component({
-  selector: 'app-population',
-  templateUrl: './population.component.html',
-  styleUrls: ['./population.component.scss']
+	selector: 'app-population',
+	templateUrl: './population.component.html',
+	styleUrls: ['./population.component.scss']
 })
 export class PopulationComponent implements OnInit {
 
-  isRhino: boolean = false;
-  elephantsMethods:any[] = [];
-  graphs = [];
+	isRhino: boolean = false;
+	populationMethods: any[] = [];
+	graphs = [];
 
-  constructor(private globalService: GlobalService) { }
+	constructor(private globalService: GlobalService) { }
 
-  ngOnInit(): void {
-    this.globalService.isRhino().subscribe((val) => {
-      this.isRhino = val;
-    });
+	ngOnInit(): void {
+		this.globalService.isRhino().subscribe((val) => {
+			this.isRhino = val;
 
-    this.elephantsMethods = [
-      "To analyze and understand the population trends of elephants, the report covers four regions in Asia and four regions in Africa, which has been mentioned already. In Asia, generally two types of counting methods have been adopted namely, camera trapping and dung analysis. Based on sightings in camera traps and indirect estimation methods, elephant count is calculated. Also, in recent years, techniques like analyzing the dung samples have also been deployed to estimate birth rates and population trends in elephants.",
-      "In the figures, the population trends have been shown and also the predicted value that could be the possible count of the elephants have been estimated. The prediction model is based on a polynomial regression model. For each graph, to understand the accuracy of the curve, the root-mean-square error and the respective R2-score have been provided.",
-    ];
+			if(this.isRhino){
+				this.populationMethods = [
+					"The population is calculated by the respective authorities through different methods such as camera trapping, dung analysis, manual counting, etc. The calculation is based on the exponential model, 𝑁𝑡 = 𝑁0𝑒𝑟𝑡 .",
+					"For our analysis, we have created a Polynomial Regression model, which fits the data that is present in a polynomial function and then predicts the output based on the input that is given.",
+					"R2 (coefficient of determination) score is the amount of the variation in the output. It is used to check how well-observed results are reproduced by the model, depending on the ratio of total deviation of results described by the model.",
+					"For calculating the Accuracy of the model, we have used the RMSE(Root Mean Squared Error) of the model. The idea is to measure how bad the model’s predictions are when compared to actual observed values."
+				];
 
-    this.graphs = [
-      {
-        title: "Elephant Population in Asia",
-        estimate: {
-          labels: [2007,2008,2009,2010,2011,2012,2013,2014,2015],
-          datasets: [
-            {
-              label: 'Botswana',
-              borderColor: '#42A5F5',
-              data: [300,330,400,400,400,400,400,400,0]
-            }
-          ]
-        },
-        predict: {
-          labels: [2007,2008,2009,2010,2011,2012,2013,2014,2015],
-          datasets: [
-            {
-              label: 'Botswana',
-              borderColor: '#42A5F5',
-              data: [300,330,400,400,400,400,400,400,0]
-            }
-          ]
-        }
-      }
-    ];
-  }
+				this.globalService.getRhinoGraph().subscribe((val) => {
+					if (val) {
+						for(let id in val){
+							this.populateGraphs(val[id]);
+							break;
+						}
+					}
+				});
+			}
+			else{
+				this.populationMethods = [
+					"To analyze and understand the population trends of elephants, the report covers four regions in Asia and four regions in Africa, which has been mentioned already. In Asia, generally two types of counting methods have been adopted namely, camera trapping and dung analysis. Based on sightings in camera traps and indirect estimation methods, elephant count is calculated. Also, in recent years, techniques like analyzing the dung samples have also been deployed to estimate birth rates and population trends in elephants.",
+					"In the figures, the population trends have been shown and also the predicted value that could be the possible count of the elephants have been estimated. The prediction model is based on a polynomial regression model. For each graph, to understand the accuracy of the curve, the root-mean-square error and the respective R2-score have been provided.",
+				];
 
+				this.globalService.getElephantGraph().subscribe((val) => {
+					if (val) {
+						for(let id in val){
+							this.populateGraphs(val[id]);
+							break;
+						}
+					}
+				});
+			}
+		});
+	}
+
+	populateGraphs(arr) {
+		let temp = [];
+		for(let item of arr){
+			temp.push(
+				{
+					title: item.title,
+					estimate: {
+						labels: item.estimate.x,
+						datasets: [
+							{
+								label: 'Model Predicted',
+								borderColor: '#E54343',
+								data: item.estimate.y_pred,
+								borderDash: [5, 5]
+							},
+							{
+								label: 'Estimated',
+								borderColor: '#42A5F5',
+								data: item.estimate.y_est
+							}
+						]
+					},
+					predict: {
+						labels: item.predict.x,
+						datasets: [
+							{
+								label: 'Prediction',
+								borderColor: '#00bb7e',
+								data: item.predict.y
+							}
+						]
+					}
+				}
+			);
+		}
+
+		this.graphs = temp;
+	}
 }
--- src/app/service/global.service.ts
+++ src/app/service/global.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Subject } from 'rxjs';
 
@@ -7,8 +8,9 @@ import { BehaviorSubject, Subject } from 'rxjs';
 export class GlobalService {
 
   private _isRhino:BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  private _elephantGraph:any;
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   isRhino(){
     return this._isRhino.asObservable();
@@ -17,4 +19,15 @@ export class GlobalService {
   setRhino(val:boolean){
     this._isRhino.next(val);
   }
+
+  getElephantGraph(){
+    return this.http.get('https://est-sis-default-rtdb.asia-southeast1.firebasedatabase.app/elephants/graphs.json');
+  }
+  
+  getRhinoGraph(){
+    return this.http.get('https://est-sis-default-rtdb.asia-southeast1.firebasedatabase.app/rhinos/graphs.json');
+  }
+
+
+
 }
